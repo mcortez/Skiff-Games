291dc31c0bc944dbde450749493de0279f913ea0
mcortez <mcortez@3100_32404.fullcoll.edu>
2011-01-18 12:23:24 -0800

    Testing adding new project via eclipse.

---
+++ sgHelloWorld/src/com/skiffgames/HelloWorld/sgHelloWorldActivity.java
@@ -0,0 +1,13 @@
+package com.skiffgames.HelloWorld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class sgHelloWorldActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
